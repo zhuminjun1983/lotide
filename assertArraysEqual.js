6207e8e96f7c7c2e5cfc1fd3c1389d960b8d831e
zhuminjun1983 <87101201+zhuminjun1983@users.noreply.github.com>
2021-08-17 17:09:56 -0700

    update arr to arr get meg

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(a,b) {
+    let out
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === b[i]) {
+            out = true
+        } else {
+            out = false
+        }
+    } if (out ===true) {
+      console.log("Assertion Passed: " + a + " === " + b) 
+    }else {
+      console.log("Assertion Failed: " + a + " !== " + b)
+    }
+}
+// TEST CODE
+assertArraysEqual(['jj','dd','ee'], [1, 2, 3])
