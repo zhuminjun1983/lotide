48032fe85375ed8bdb7337cd2e0b97a7774ab975
zhuminjun1983 <87101201+zhuminjun1983@users.noreply.github.com>
2021-08-23 13:08:05 -0700

    new

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const head = require('../head');  
+const assertEqual = require('../assertEqual')
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
